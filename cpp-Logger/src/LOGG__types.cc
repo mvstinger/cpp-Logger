3a08a7bd2571781163581352b1e4378f64463b1d
Michael Stinger <mvstinger@gmail.com>
2016-11-13 16:25:55 -0500

    Changed file names & specified inheritance for xLogger types
    + Added additional underscore ('_') between project identifier ('LOGG')
    and file identifier ('types').
    + Specified 'public' inheritance for ConsoleLogger and FileLogger when
    inhereting from LoggerBase.

---
+++ cpp-Logger/src/LOGG__types.cc
@@ -0,0 +1,154 @@
+/*
+ * LOGG_types.cc
+ *
+ *  Created on: Nov 13, 2016
+ *      Author: mvstinger
+ */
+
+
+
+
+#include "../LOGG_types.h"
+
+
+
+namespace Logger {
+
+
+
+LoggerInterface::LoggerInterface(void) {};
+
+LoggerInterface::LoggerInterface(const LogLevel lvl) :
+		log_level_(lvl) {};
+
+LoggerInterface::LoggerInterface(const LogLevel lvl, ostream* ptr) :
+		ostream_ptr_(ptr),
+		log_level_(lvl) {};
+
+
+
+LoggerBase::LoggerBase(void) {};
+
+LoggerBase::LoggerBase(const LogLevel lvl) :
+		LoggerInterface(lvl) {};
+
+LoggerBase::LoggerBase(const LogLevel lvl, ostream* ptr) :
+		LoggerInterface(lvl, ptr) {};
+
+int LoggerBase::critical(const string msg) const {
+	int error_state = 0;
+	this->log_(LOG_CRITICAL, msg);
+	return error_state;
+}
+
+int LoggerBase::error(const string msg) const {
+	int error_state = 0;
+	this->log_(LOG_ERROR, msg);
+	return error_state;
+}
+
+int LoggerBase::warning(const string msg) const {
+	int error_state = 0;
+	this->log_(LOG_WARNING, msg);
+	return error_state;
+}
+
+int LoggerBase::info(const string msg) const {
+	int error_state = 0;
+	this->log_(LOG_INFO, msg);
+	return error_state;
+}
+
+int LoggerBase::debug(const string msg) const {
+	int error_state = 0;
+	this->log_(LOG_DEBUG, msg);
+	return error_state;
+}
+
+LogLevel LoggerBase::get_level(void) const {
+	return this->log_level_;
+}
+
+int LoggerBase::set_level(const LogLevel lvl) {
+	int error_state = 0;
+	this->log_level_ = lvl;
+	return error_state;
+}
+
+int LoggerBase::log_(const LogLevel lvl, const string msg) const {
+	int error_state = 0;
+	if(lvl >= this->log_level_) {
+		 *(this->ostream_ptr_) << now_string() << "\t" << to_string(lvl) << "\t" << msg << std::endl;
+	}
+	return error_state;
+}
+
+
+
+NullLogger::NullLogger(void) :
+		LoggerBase(LOG_SILENT) {};
+
+NullLogger::~NullLogger(void) {};
+
+//	Do not allow change of log level (this hides parent class method)
+int NullLogger::set_level(const LogLevel irrelevant) { return 1; }
+
+
+
+ConsoleLogger::ConsoleLogger(void) :
+		LoggerBase(LOGG__DEFAULT_LOG_LEVEL, (ostream*)stdout) {};
+
+ConsoleLogger::ConsoleLogger(const LogLevel lvl) :
+		LoggerBase(lvl, (ostream*)stdout) {};
+
+ConsoleLogger::~ConsoleLogger(void) {};
+
+
+
+FileLogger::FileLogger(const string filepath) {
+	this->ostream_target_ = ofstream(filepath.c_str());
+	this->log_level_ = LOGG__DEFAULT_LOG_LEVEL;
+	this->ostream_ptr_ = &(this->ostream_target_);
+}
+
+FileLogger::FileLogger(const LogLevel lvl, const string filepath) {
+	this->ostream_target_ = ofstream(filepath.c_str());
+	this->log_level_ = lvl;
+	this->ostream_ptr_ = &(this->ostream_target_);
+}
+
+FileLogger::~FileLogger(void) {
+	this->ostream_ptr_ = NULL;
+	this->ostream_target_.close();
+}
+
+
+
+string now_string(void) {
+	return string("YYYYmmdd_HHMMSS");
+}
+
+
+
+string to_string(const LogLevel lvl) {
+	switch(lvl) {
+	case LOG_SILENT:
+		return "SILENT";
+	case LOG_CRITICAL:
+		return "CRITICAL";
+	case LOG_ERROR:
+		return "ERROR";
+	case LOG_WARNING:
+		return "WARNING";
+	case LOG_INFO:
+		return "INFO";
+	case LOG_DEBUG:
+		return "DEBUG";
+	default:
+		return "UNK";
+	}
+}
+
+
+
+};
